0ee17edfe5227f5aeb89f4fd06ec12859937d6d5
Robert Clarke <r.jason.clarke@gmail.com>
2020-09-27 22:37:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,15 @@ class Dog
   def bark
     puts "Woof!"
   end
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
 
+  def name
+    this_dogs_name
+  end
 end
 
 fido = Dog.new
-fido.name = ("Fido")
+fido.name = "Fido"
 fido.bark
